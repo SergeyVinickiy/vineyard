c2a9d512a20d6ab9ab2d7508b6e6870ac519bbbe
SergeyV1 <sergey.vinickiy@verifone.com>
2020-06-25 20:12:20 +0300

    delete bundle

--- src/main/java/com/vinickiy/vineyard/bundles/dao/BundlesDao.java
+++
@@ -1,7 +0,0 @@
-package com.vinickiy.vineyard.bundles.dao;
-
-import com.vinickiy.vineyard.model.entity.Bundle;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BundlesDao extends CrudRepository<Bundle, Long> {
-}
